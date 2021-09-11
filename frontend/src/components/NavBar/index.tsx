8caa1e094d192cbc73df3095f6f1d7a7960c0d1f
RobsonPereira13 <robsonpereirape@hotmail.com>
2021-09-11 15:05:35 -0300

    firts request

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,5 +1,6 @@
 
 import logojava from 'assets/img/logojava.png'
+import { Link } from 'react-router-dom';
 
 
 const NavBar =()=> {
@@ -7,7 +8,10 @@ const NavBar =()=> {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
         <div className="container">
           <nav className="my-2 my-md-0 mr-md-3">
+            <Link to="/">
+
             <img src={logojava} alt="DevSuperior" width="120" />
+            </Link>
           </nav>
         </div>
       </div>
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+
+
 const Home =()=> {
     return (
-        <div >
-            <h1>Home</h1>
-      </div>
+        <>
+        <NavBar/>
+        <div className="container">
+        <div className="jumbotron">
+            <h1 className="display-4">Vendas</h1>
+            <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+            <hr/>
+            <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+          
+            <Link className="btn btn-outline-warning" to="/dashboard"> Acesse o Dashboard</Link>
+        </div>
+    </div>
+    <Footer/>
+    </>
     );
 }
 
