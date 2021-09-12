1a21a397214338af43236eb5fccbb04d0a69150c
RobsonPereira13 <robsonpereirape@hotmail.com>
2021-09-12 19:23:43 -0300

    pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
+import Pagination  from "components/Pagination";
 
 
 
 
 const DataTable =()=> {
+    const [activePage, setActivePage] = useState(0);
     const [page, setPage] = useState<SalePage>({
         first:true,
         last:true,
@@ -16,12 +18,17 @@ const DataTable =()=> {
         totalPages:0
     });
     useEffect(()=>{
-axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
 .then(response =>{
     setPage(response.data)
 })
-    },[])
+    },[activePage])
+    const changePage = (index:number)=>{
+        setActivePage(index);
+    }
     return (
+        <>
+        <Pagination page={page} onPagechange={changePage}/>
         <div className="table-responsive">
         <table className="table table-striped table-sm">
             <thead>
@@ -47,6 +54,7 @@ axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
             </tbody>
         </table>
     </div>
+    </>
     );
 }
 
